1ed482a66b403864742d75a8235120ad50252fe5
Guillaume Tricknot <guillaumetricknot@gmail.com>
2022-10-04 08:55:51 +0200

    Initialisation de Git

---
+++ PythonApplicationLabo/Anonym.py
@@ -0,0 +1,107 @@
+import glob 
+import numpy as np
+import pandas as pd
+#from edf import *
+#from resu import *
+import sys
+import os
+
+
+def AnonymiseEDF(EDFName):                                  #fonction permettant d'anonymiser les données du patient du fichier EDF
+    EDF = open(EDFName, "rb")                               #r = read, + signifie écrire dedans et b en bit
+    EDF.read(8)
+    EDFText = EDF.read(160).decode('unicode_escape')        #telle lettre egal telle code en binaire (unicode)
+    EDF.close() 
+    EDF = open(EDFName, "r+")
+    EDF.seek(8)                                             #replacer à partir du caractère 8
+    sss = "x" * 160
+    EDF.write(sss)
+    EDF.close()
+    
+    EDF = open(EDFName, "rb")
+    EDF.read(168)
+    EDFText1 = EDF.read(8).decode('unicode_escape')
+    EDF.close() 
+    EDF = open(EDFName, "r+")
+    EDF.seek(168)
+    sss = "x" *8
+    EDF.write(sss)
+    EDF.close()
+
+    print("EDFTXT: ", EDFText)
+    print("EDFTXT1: ", EDFText1)
+    return EDFText,EDFText1
+
+def AnonymiseResu(resuName):                                #fonction permettant d'anonymiser les données du patient du fichier resu
+    print("Anonymiseresu")                                      
+    resu=open(resuName,'r+' )
+    resu.seek(144)
+    sss = "x" * 22
+    resu.write(sss)
+    resu.seek(1922)
+    sss = "x" * 87
+    resu.write(sss)
+    resu.close()
+    print(resu)
+    return      
+
+def UnAnonymiseEDF(EDFName, EDFText):                      #fonction permettant de déanonymiser les données du patient du fichier EDF
+    print(len(EDFText))
+    EDF = open(EDFName, "r+")
+    EDF.seek(8)
+    EDF.write(EDFText)
+    EDF.close()
+
+    #print(EDFText)
+    #print(len(EDFText))
+    #EDF = open(EDFName, "r+")
+    #EDF.seek(168)
+    #EDF.write(EDFText[16:17])
+    #EDF.write(EDFText[14:15])
+    #EDF.write(EDFText[12:13])
+    #EDF.close()
+    #return
+
+def UnAnonymiseResu(resuName, RawFileName, resuText ):      #fonction permettant de déanonymiser les données du patient du fichier resu
+    print("iunAnonymiseresu")
+    resu=open(resuName,'r+' )
+    resu.seek(144)
+    resu.write(RawFileName)
+    resu.seek(1922)
+    resu.write(resuText)
+    resu.close() 
+    return
+
+def saveData(resuName,RawFileName,resuText):                #fonction permettant de sauvegarder les données dans un .txt
+    dataName = resuText+'.txt'                              #crée un fichier txt si non existant
+    dataFile = open(dataName, 'w')                          #w pour whrite, vérifier si avec w le programme crée un fichier texte
+    dataFile.write(resuName) 
+    dataFile.write('\t')                                    #aller à la ligne \t faire un tab 
+    dataFile.write(RawFileName)
+    dataFile.write('\t')
+
+    EDF = open(RawFileName, "rb")       #Permet de prendre les infos du fichier EDF                    
+    EDF.read(8)
+    EDFText = EDF.read(168).decode('unicode_escape')    
+    EDF.close()
+    dataFile.write(EDFText[0:23])
+    dataFile.write('\t')
+    dataFile.write(EDFText[160:168])
+   
+    dataFile.write('\t')
+    dataFile.write('ID 1')
+    dataFile.close()
+    return  
+
+def ChangeNameToAnonyme(resuName,RawFileName):                       #fonction permettant d'anonymiser le nom du fichier resu et EDF
+    os.rename(resuName,'resuAnonyme.resu')                  #Ne pas mettre resuName entre quote
+    os.rename(RawFileName,'RawAnonyme.EDF')
+    return
+
+def ChangeAnonymeToName(txt) :     #remettre en paramètre resuAnonyme et RawAnonyme
+    txt=open(txt,'r')
+    txt.read(160)
+    resuName = txt.read(22).decode('unicode_escape')
+    print (resuName)
+    #os.rename(resuAnonyme,'')
+    #os.rename(RawAnonyme,'')
---
+++ PythonApplicationLabo/PythonApplicationLabo.py
@@ -0,0 +1,89 @@
+from os import read
+import mne
+import numpy as np
+import matplotlib.pyplot as plt
+from Anonym import *
+
+def readEDF(EDFName):               #fonction permettant de lire les fichier EDF, le fichier doit se trouver dans le dossier du programme 
+    ErasmeToMneEdf(EDFName)
+    raw = mne.io.read_raw_edf(EDFName, preload=True)
+    print(raw)
+    print('Chan =', raw.ch_names)   #Ligne qui renvoit le nom de tous les EEG
+    print('Sampling frequency =', raw.info['sfreq'])
+    print('Data shape (channels, times) =', raw._data.shape)
+    print(raw.info)
+    #raw.pick_types(eeg=True, eog=False)
+    raw.pick(raw.ch_names[5])                 #fonctionne
+                  
+    print('Chan =', raw.ch_names)
+    #raw.plot(start=30, duration=10,scalings=dict(eeg=1e-4,resp=1e3,eog=1e-4,emg=1e-7,misc=1e-1))
+    raw.plot(duration=30, start=10, n_channels=1, block=True,title='test') #affiche le graph des différents capteurs
+    MneToErasmeEdf(EDFName)
+    return raw
+def ErasmeToMneEdf(EDFName):        #fonction permettant de changer dans le format des caractère par des points : format historique de erasme
+    fid = open(EDFName, "r+")       #r pour (lecture) et + pour écrire dedans 
+    fid.seek(170)                   #seek = chercher 
+    fid.write(".")
+    fid.seek(173)
+    fid.write(".")
+    fid.seek(178)
+    fid.write(".")
+    fid.seek(181)
+    fid.write(".")
+    fid.close()
+    return
+def MneToErasmeEdf(EDFName):        #fonction permettant de changer dans le format des caractère par des points : format historique de erasme
+    fid = open(EDFName, "r+")
+    fid.seek(170)
+    fid.write("/")
+    fid.seek(173)
+    fid.write("/")
+    fid.seek(178)
+    fid.write(":")
+    fid.seek(181)
+    fid.write(":")
+    fid.close()
+def readEDFHeader(EDFName):         #fonction permettant de lire l'entête (Header) ce qui donne les info sur le patient,...
+    ErasmeToMneEdf(EDFName)
+    EDF = open(EDFName, "rb")
+    header = {}
+    header['version'] = EDF.read(8).decode('unicode_escape')
+    header['patient'] = EDF.read(80).decode('unicode_escape')
+    header['recording'] = EDF.read(80).decode('unicode_escape')
+    header['startDate'] = EDF.read(16).decode('unicode_escape')
+    header['headerSize'] = int(EDF.read(8).decode('unicode_escape'))
+    header['reserved'] = EDF.read(44).decode('unicode_escape')
+    header['recordsNumber'] = int(EDF.read(8).decode('unicode_escape'))
+    header['recordDuration'] = int(EDF.read(8).decode('unicode_escape'))
+    header['signalsNumber'] = int(EDF.read(4).decode('unicode_escape'))
+    header['labels'] = list(chunkPrint(EDF.read(header['signalsNumber']*16).decode('unicode_escape'), 16))
+    header['transducers'] = list(chunkPrint(EDF.read(header['signalsNumber']*80).decode('unicode_escape'), 80))
+    header['physDim'] = EDF.read(header['signalsNumber']*8).decode('unicode_escape')
+    header['physMin'] = list(chunkPrint(EDF.read(header['signalsNumber']*8).decode('unicode_escape'), 8))
+    header['physMin'] = np.array(header['physMin'], dtype=np.float32)
+    header['physMax'] = list(chunkPrint(EDF.read(header['signalsNumber']*8).decode('unicode_escape'), 8))
+    header['physMax'] = np.array(header['physMax'], dtype=np.float32)
+    header['digMin'] = list(chunkPrint(EDF.read(header['signalsNumber']*8).decode('unicode_escape'), 8))
+    header['digMin'] = np.array(header['digMin'], dtype=np.float32)
+    header['digMax'] = list(chunkPrint(EDF.read(header['signalsNumber']*8).decode('unicode_escape'), 8))
+    header['digMax'] = np.array(header['digMax'], dtype=np.float32)
+    header['prefiltering'] = list(chunkPrint(EDF.read(header['signalsNumber']*80).decode('unicode_escape'), 80))
+    header['samplesNumber'] = list(chunkPrint(EDF.read(header['signalsNumber']*8).decode('unicode_escape'), 8))
+    header['reserved2'] = list(chunkPrint(EDF.read(header['signalsNumber']*32).decode('unicode_escape'), 32))
+    EDF.close() 
+    EDF = open(EDFName, "rb")
+    header['fullHeader'] = EDF.read(header['headerSize']).decode('unicode_escape')
+    EDF.close()
+    print(header)
+    return header       
+def chunkPrint(string, length):     #fonction permettant de séparer les labels et les mettre dans une liste
+    return (string[0+i:length+i].strip() for i in range(0, len(string), length))
+
+
+#readEDFHeader("PX428090.EDF")
+#saveData("PERSONX0-20220928.resu","PX428090.EDF","Anonym")
+#AnonymiseEDF("PX428090.EDF")
+#AnonymiseResu("PERSONX0-20220928.resu")                             #REMARQUE : la date n'est pas anonymisée dans le fichier resu
+#ChangeNameToAnonyme("PERSONX0-20220928.resu","PX428090.EDF")
+#UnAnonymiseEDF("RawAnonyme.EDF","Anonym.txt")
+ChangeAnonymeToName("Anonym.txt")
