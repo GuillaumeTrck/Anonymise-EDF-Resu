95c86148fc996d36f65b906b2ba2718011683181
Guillaume <guillaumetricknot@gmail.com>
2022-10-11 16:09:48 +0200

    [NEW] Ajout de condition (rajout edf/resu)

--- Anonym.py
+++ Anonym.py
@@ -12,28 +12,36 @@ import os
 import argparse
 
 def AnonymiseEDF(EDFName):                                  #fonction permettant d'anonymiser les donn�es du patient du fichier EDF
-    EDF = open(EDFName, "r+")
-    EDF.seek(8)                                             #replacer � partir du caract�re 8
-    sss = "x" * 168
-    EDF.write(sss)
-    EDF.close()
+    x='xxxxxxxxxxxxxxxxxxxxxx.EDF'
+    if x in EDFName:
+        print("Fichier EDF déjà anonyme")
+    else:
+        EDF = open(EDFName, "r+")
+        EDF.seek(8)                                             #replacer � partir du caract�re 8
+        sss = "x" * 168
+        EDF.write(sss)
+        EDF.close()
     return 
 
 def AnonymiseResu(resuName):                                #fonction permettant d'anonymiser les donn�es du patient du fichier resu                                     
-    resu=open(resuName,'r+')
-    resu.seek(24)                                           #anonymise la date
-    sss = "x"*10
-    resu.write(sss)
-    resu.seek(48)                                           #anonymise la chambre
-    sss="x"*4
-    resu.write(sss)
-    resu.seek(144)
-    sss = "x" * 22
-    resu.write(sss)
-    resu.seek(1922)
-    sss = "x" * 87
-    resu.write(sss)
-    resu.close()
+    x='resuAnonyme'
+    if x in resuName:
+        print("Fichier resu déjà anonyme")
+    else:
+        resu=open(resuName,'r+')
+        resu.seek(24)                                           #anonymise la date
+        sss = "x"*10
+        resu.write(sss)
+        resu.seek(48)                                           #anonymise la chambre
+        sss="x"*4
+        resu.write(sss)
+        resu.seek(144)
+        sss = "x" * 22
+        resu.write(sss)
+        resu.seek(1922)
+        sss = "x" * 87
+        resu.write(sss)
+        resu.close()
     return      
 
 def UnAnonymiseEDF(EDFName,matriceEDF):                    #fonction permettant de d�anonymiser les donn�es du patient du fichier EDF
@@ -55,66 +63,72 @@ def UnAnonymiseResu(resuName,matriceresu):                   #fonction permettan
     return
                                          
 def saveDataresu(resuName,resuData,ID):
-    
+    x='resuAnonyme'
+    if x in resuName:
+        print("Fichier resu déjà sauvé")
+    else:
     # Writing data
-    dataFile = open(resuData, 'a')
-    dataFile.write(resuName) 
-    dataFile.write('\t')
-    fid = open(resuName, "rb")                           
-    resu = {}
-    fid.seek(24)
-    resu['ExamDate'] = fid.read(10).decode('unicode_escape')
-    dataFile.write(resu['ExamDate'])
-    dataFile.write('\t')
-    fid.seek(48)
-    resu['Room'] = fid.read(4).decode('unicode_escape')  
-    dataFile.write(resu['Room'])
-    dataFile.write('\t')
-    fid.seek(144)
-    resu['RawFileName'] = fid.read(22).decode('unicode_escape') 
-    dataFile.write(resu['RawFileName'])
-    dataFile.write('\t')
-    fid.seek(1922)
-    resu['FileNumber'] =  fid.read(22).decode('unicode_escape')  
-    dataFile.write(resu['FileNumber'])
-    dataFile.write('\t')
-    resu['Name'] =  fid.read(27).decode('unicode_escape')  
-    dataFile.write(resu['Name'])
-    dataFile.write('\t')
-    resu['FirstName'] =  fid.read(27).decode('unicode_escape')  
-    dataFile.write(resu['FirstName']) 
-    dataFile.write('\t')
-    resu['BirthDate'] =  fid.read(10).decode('unicode_escape')  
-    dataFile.write(resu['BirthDate'])
-    dataFile.write('\t')
-    resu['Sex'] = fid.read(1).decode('unicode_escape')
-    dataFile.write(resu['Sex'])
-    dataFile.write('\t')
-    dataFile.write(str(ID))
-    dataFile.write('\n')
-    fid.close()
-    dataFile.close()
+        dataFile = open(resuData, 'a')
+        dataFile.write(resuName) 
+        dataFile.write('\t')
+        fid = open(resuName, "rb")                           
+        resu = {}
+        fid.seek(24)
+        resu['ExamDate'] = fid.read(10).decode('unicode_escape')
+        dataFile.write(resu['ExamDate'])
+        dataFile.write('\t')
+        fid.seek(48)
+        resu['Room'] = fid.read(4).decode('unicode_escape')  
+        dataFile.write(resu['Room'])
+        dataFile.write('\t')
+        fid.seek(144)
+        resu['RawFileName'] = fid.read(22).decode('unicode_escape') 
+        dataFile.write(resu['RawFileName'])
+        dataFile.write('\t')
+        fid.seek(1922)
+        resu['FileNumber'] =  fid.read(22).decode('unicode_escape')  
+        dataFile.write(resu['FileNumber'])
+        dataFile.write('\t')
+        resu['Name'] =  fid.read(27).decode('unicode_escape')  
+        dataFile.write(resu['Name'])
+        dataFile.write('\t')
+        resu['FirstName'] =  fid.read(27).decode('unicode_escape')  
+        dataFile.write(resu['FirstName']) 
+        dataFile.write('\t')
+        resu['BirthDate'] =  fid.read(10).decode('unicode_escape')  
+        dataFile.write(resu['BirthDate'])
+        dataFile.write('\t')
+        resu['Sex'] = fid.read(1).decode('unicode_escape')
+        dataFile.write(resu['Sex'])
+        dataFile.write('\t')
+        dataFile.write(str(ID))
+        dataFile.write('\n')
+        fid.close()
+        dataFile.close()
 
     return
 
 def saveDataEDF(RawFileName, EDFData,ID):
-
+    x='xxxxxxxxxxxxxxxxxxxxxx.EDF'
+    if x in RawFileName:
+        print("Fichier EDF déjà sauvé")
+    else:
     # Writing data
-    dataFile = open(EDFData, "a")
-    dataFile.write(RawFileName)
-    dataFile.write('\t')
-    EDF = open(RawFileName, "rb")                          
-    EDF.read(8)
-    EDFText = EDF.read(168).decode('unicode_escape')    
-    EDF.close()
-    dataFile.write(EDFText[0:80])
-    dataFile.write(EDFText[80:160])
-    dataFile.write('\t')
-    dataFile.write(EDFText[152:168])
-    dataFile.write('\t')
-    dataFile.write(str(ID))
-    dataFile.write('\n')
-    dataFile.close()
+        dataFile = open(EDFData, "a")
+        dataFile.write(RawFileName)
+        dataFile.write('\t')
+        EDF = open(RawFileName, "rb")                          
+        EDF.read(8)
+        EDFText = EDF.read(168).decode('unicode_escape')    
+        EDF.close()
+        dataFile.write(EDFText[0:80])
+        dataFile.write(EDFText[80:160])
+        dataFile.write('\t')
+        dataFile.write(EDFText[152:168])
+        dataFile.write('\t')
+        dataFile.write(str(ID))
+        dataFile.write('\n')
+        dataFile.close()
 
     return
 
@@ -165,10 +179,19 @@ def CheckID():
     return max([EDFID,resuID])   
 
 def ChangeNameToAnonyme(resuName, rawFileName, ID): 
-    resuAnonymeFileName="resuAnonyme{0}.resu".format(ID)                      
-    os.rename(resuName,resuAnonymeFileName)   
-    rawAnonymeFileName = "RawAnonyme{0}.EDF".format(ID)               
-    os.rename(rawFileName,rawAnonymeFileName)
+    
+    print(resuName)
+    print(rawFileName)
+    x='resuAnonyme'
+    if x in resuName:
+        print("Noms resu déjà anonyme")
+        return
+    else:
+        resuAnonymeFileName="resuAnonyme{0}.resu".format(ID)                      
+        os.rename(resuName,resuAnonymeFileName)   
+        rawAnonymeFileName = "RawAnonyme{0}.EDF".format(ID)               
+        os.rename(rawFileName,rawAnonymeFileName)
+        print("J'ai bien changé le nom")
     return [resuAnonymeFileName,rawAnonymeFileName]
 
 def ChangeAnonymeToName(resuAnonyme,rawAnonyme, matriceEDF,matriceresu,i) :
--- PythonApplicationLabo.py
+++ PythonApplicationLabo.py
@@ -6,102 +6,114 @@ from Anonym import *
 import glob
 import argparse
 
-from utils import initLogs
-
-
 parser=argparse.ArgumentParser(description='Save Data EDF and resu')
-parser.add_argument('-f','--file', required=True, help="File name with EDF and resu")
+parser.add_argument('-f','--file', required=False, help="File name with EDF and resu")
 parser.add_argument('-d','--EDFData', required=True, help="Edf data text name")
 parser.add_argument('-v','--resuData', required=True, help="resu data text name")
-parser.add_argument('-u','--unAnomyse', required=True, help="unanomyse edf and resu in file")
+parser.add_argument('-u','--unAnomyse', required=False, help="unanomyse edf and resu in file")
 
 args=parser.parse_args()
 
 
 file=args.file
+fileA=args.unAnomyse
 EDFData=args.EDFData
 resuData=args.resuData
 
-# os.chdir(file)
 
-# EDFList=glob.glob("*.EDF")
-# resuList=glob.glob("*.resu")
-# print(type(resuList))
-# print(resuList)
+#6 unanonymiser edf et resu (nom et données)
+def UN():    
+    os.chdir(fileA)
+    EDFList=glob.glob("*.EDF")
+    resuList=glob.glob("*.resu")
 
-# test=FirstLineEDF(EDFData)
-# test1=FirstLineresu(resuData)
-# if test and test1:
-#     ID = 1
-# else :
-#     ID = CheckID()
+    matriceEDF=np.loadtxt(EDFData,delimiter='\t',comments=None,encoding='utf-8',dtype='U',skiprows=1,ndmin=2)
+    matriceresu=np.loadtxt(resuData,delimiter='\t',comments=None,encoding='utf-8',dtype='U',skiprows=1,ndmin=2)
+    i=0
+    for id in EDFList:
 
-# for resu in resuList:
-#     saveDataresu(resu,resuData,ID)
-    
-    #1. récupérer edfname
-    # fid = open(resu, "rb")                                                   
-    # fid.seek(144)
-    # RawFileName= fid.read(22).decode('unicode_escape')
-    # RawFileNameStrip=RawFileName.replace(" ","")
-    # fid.close()
-    
-    #2. trouver l'edf 
-    # term='.EDF'
-    # RawFileNameStripTerm=RawFileNameStrip + term
-    # EDFName=glob.glob(RawFileNameStripTerm)
-    # print(EDFName)
-    #3 savedataedf
-    # saveDataEDF(RawFileNameStripTerm,EDFData,ID)
+        #1 récupérer ID du fichier
+        
+        
+        EDFAnonymeName=EDFList[i]
+        EDFAnonymeName=EDFAnonymeName.replace(".EDF","")
+        IDFichierAnonyme=EDFAnonymeName[10:]
+        #2 voir où se trouve le fichier dans datafile
+        i=CheckInDataFile(matriceEDF,IDFichierAnonyme)
+        
+        
+        #3 changer les noms anonymes et les données des edf et resu
+        terminasonEDF='.EDF'
+        terminaisonResu='.resu'
+        originalEDFName=glob.glob('RawAnonyme'+IDFichierAnonyme+terminasonEDF)
+        originalResuName=glob.glob('resuAnonyme'+IDFichierAnonyme+terminaisonResu)
+        print(originalEDFName)
+        print(originalResuName)
+        i=int(i)
+        originalsNames=ChangeAnonymeToName(originalResuName[0],originalEDFName[0], matriceEDF,matriceresu,i)
+        UnAnonymiseEDF(originalsNames[1],matriceEDF)
+        UnAnonymiseResu(originalsNames[0],matriceresu)
+        i=int(i)
+        print(type(i))
+        i=i+1
+        print(i)
+    return
 
-    #4 Anonymise edf et resu (nom et données)
-    # AnonymiseEDF(RawFileNameStripTerm)
-    # AnonymiseResu(resu)
-    # anonymeNames=ChangeNameToAnonyme(resu,RawFileNameStripTerm,ID)
+def AN():    
+    os.chdir(file)
 
-    #5 unanonymiser edf et resu (nom et données)
+    EDFList=glob.glob("*.EDF")
+    resuList=glob.glob("*.resu")
+    print(type(resuList))
+    print(resuList)
 
-os.chdir(file)
-EDFList=glob.glob("*.EDF")
-resuList=glob.glob("*.resu")
+    test=FirstLineEDF(EDFData)
+    test1=FirstLineresu(resuData)
+    if test and test1:
+        ID = 1
+    else :
+        ID = CheckID()
 
-matriceEDF=np.loadtxt(EDFData,delimiter='\t',comments=None,encoding='utf-8',dtype='U',skiprows=1,ndmin=2)
-matriceresu=np.loadtxt(resuData,delimiter='\t',comments=None,encoding='utf-8',dtype='U',skiprows=1,ndmin=2)
-i=0
-for id in EDFList:
+    for resu in resuList:
+        saveDataresu(resu,resuData,ID)
+        
+        #1. récupérer edfname
+        fid = open(resu, "rb")                                                   
+        fid.seek(144)
+        RawFileName= fid.read(22).decode('unicode_escape')
+        RawFileNameStrip=RawFileName.replace(" ","")
+        fid.close()
+        
+        #2. trouver l'edf 
+        term='.EDF'
+        RawFileNameStripTerm=RawFileNameStrip + term
+        EDFName=glob.glob(RawFileNameStripTerm)
+        print(EDFName)
+        #3 savedataedf
+        saveDataEDF(RawFileNameStripTerm,EDFData,ID)
 
-    #1 récupérer ID du fichier
-    
-    
-    EDFAnonymeName=EDFList[i]
-    EDFAnonymeName=EDFAnonymeName.replace(".EDF","")
-    IDFichierAnonyme=EDFAnonymeName[10:]
-    #2 voir où se trouve le fichier dans datafile
-    i=CheckInDataFile(matriceEDF,IDFichierAnonyme)
-    
-    
-    #3 changer les noms anonymes et les données des edf et resu
-    terminasonEDF='.EDF'
-    terminaisonResu='.resu'
-    originalEDFName=glob.glob('RawAnonyme'+IDFichierAnonyme+terminasonEDF)
-    originalResuName=glob.glob('resuAnonyme'+IDFichierAnonyme+terminaisonResu)
-    print(originalEDFName)
-    print(originalResuName)
-    i=int(i)
-    originalsNames=ChangeAnonymeToName(originalResuName[0],originalEDFName[0], matriceEDF,matriceresu,i)
-    UnAnonymiseEDF(originalsNames[1],matriceEDF)
-    UnAnonymiseResu(originalsNames[0],matriceresu)
-    i=int(i)
-    print(type(i))
-    i=i+1
-    print(i)
+        #4 Anonymise edf et resu (nom et données)
+        AnonymiseEDF(RawFileNameStripTerm)
+        AnonymiseResu(resu)
+        anonymeNames=ChangeNameToAnonyme(resu,RawFileNameStripTerm,ID)
 
-    #6 incrémenter l'ID
-    # ID=int(ID)
-    # ID=ID+1
+        #5 incrémenter l'ID
+        ID=int(ID)
+        ID=ID+1
+    return
+
+if (args.unAnomyse):
+    UN()
+
+elif(args.file):
+    AN()
 
 
-    
+
+
+
+
+
 
 
 
